8e39f40b5a43e5e672639db4c2b4b5131d1af45c
Tran Quang Vinh Dinh Quoc Huy <vinhtransn1998@gmail.com>
2026-05-12 20:15:09 +0700

    Build login and dashboard admin

---
+++ ASPNET-DT23TT80161-LIEUICHQUANH-OANGTHITHANH-BUIPHAMTHUYTRANG-GIOITHIEUBADONG/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaDongTourism.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("AdminUser") != null)
+            {
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "123456")
+            {
+                HttpContext.Session.SetString("AdminUser", username);
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }
+
+            ViewBag.Error = "Sai tài khoản hoặc mật khẩu";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("AdminUser");
+            return RedirectToAction("Login", "Account", new { area = "Admin" });
+        }
+    }
+}
---
+++ ASPNET-DT23TT80161-LIEUICHQUANH-OANGTHITHANH-BUIPHAMTHUYTRANG-GIOITHIEUBADONG/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using BaDongTourism.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaDongTourism.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("AdminUser") == null)
+            {
+                return RedirectToAction("Login", "Account", new { area = "Admin" });
+            }
+
+            ViewBag.TotalPosts = _context.Posts.Count();
+            ViewBag.TotalCategories = _context.Categories.Count();
+            ViewBag.TotalContacts = _context.Contacts.Count();
+
+            return View();
+        }
+    }
+}
